4ae52fdeb416d415a8f0ae114c6d91dc5d2e73a8
xunichanghuan <zhpjake@vip.qq.com>
2023-02-25 21:30:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
     # 酷推skey和server酱sckey和企业微信设置，只用填一个其它留空即可
     print(datas.get("SKEY", []))
     print(datas.get("SKEY"))
-    if datas.get("SKEY") != ""
+    if datas.get("SKEY") != ''
         print("ss")
     else:
         print("00")
