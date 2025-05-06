9dcd85d0750549687f46bd4f2f46067d4a6ebd53
Bellill <1095388495@qq.com>
2025-05-06 18:18:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class MiMotion():
     def push_telegram(self,msg):
         try:
             print("\nTelegram 推送开始")
-            send_data = {"chat_id": tg_user_id, "text": title + '\n\n'+content, "disable_web_page_preview": "true"}
+            send_data = {"chat_id": tg_user_id, "text": + '【小米运动步数修改】' + '\n\n'+content, "disable_web_page_preview": "true"}
             response = requests.post(
                 url=f'https://api.telegram.org/bot{tg_bot_token}/sendMessage', data=send_data)
             print(response.json()['ok'])
