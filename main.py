1c2522202585e411cb1e3f1ba388e7191bdeb157
xunichanghuan <zhpjake@vip.qq.com>
2025-10-28 09:24:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ if __name__ == "__main__":
             _check_item = datas.get("MIMOTION", [])[i]
             #print(_check_item)
             #msg += MiMotion(check_item=_check_item).main()
-            #print(MiMotion(check_item=_check_item).main())
+            print(MiMotion(check_item=_check_item).main())
         #print(msg)
         # 酷推skey和server酱sckey和企业微信设置，只用填一个其它留空即可
         if datas.get("SKEY"):
